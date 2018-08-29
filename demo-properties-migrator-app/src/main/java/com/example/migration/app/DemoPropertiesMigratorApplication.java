c0f42fbbdb57211ef1bfb68db3ab67f6a8a48250
Stephane Nicoll <snicoll@pivotal.io>
2018-08-29 09:50:20 +0200

    Initial import

---
+++ demo-properties-migrator-app/src/main/java/com/example/migration/app/DemoPropertiesMigratorApplication.java
@@ -0,0 +1,12 @@
+package com.example.migration.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPropertiesMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPropertiesMigratorApplication.class, args);
+	}
+}
